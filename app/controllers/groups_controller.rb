9c45dcfba7805b6ca3b64e4987170a28376b1c4c
somexin <xin7722406@gmail.com>
2017-04-22 18:52:47 +0800

    people can't see edit button unless he is group owner

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,4 @@
 class GroupsController < ApplicationController
-  class GroupsController < ApplicationController
   before_action :authenticate_user! , only: [:new, :create]
   def index
     @groups = Group.all
@@ -21,18 +20,15 @@ def edit
 
 
  def create
-    @group = Group.new(group_params)
-    @group.user = current_user
+   @group = Group.new(group_params)
+   @group.user = current_user
 
-    if @group.save
-      redirect_to groups_path
-    else
-      render :new
-    end
-  end
-
-
-
+   if @group.save
+     redirect_to groups_path
+   else
+     render :new
+   end
+end
 
 
   def update
